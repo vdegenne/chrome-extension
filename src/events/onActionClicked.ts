4a710a739cdc196a0999bfcec07974109ae3d460
Valentin Degenne <vdegenne@gmail.com>
2025-10-22 19:39:01 +0200

    0.1.13

--- src/events/onActionClicked.ts
+++ src/events/onActionClicked.ts
@@ -1,9 +1,10 @@
 import {DEBUG} from '../debug.js'
 
 /**
- * Called when the extension badge is click in the toolbar.
- * It only works if you set `"action"` in the manifest
- * Not when Chrome restarts.
+ * Called when the extension badge is clicked in the toolbar.
+ * It only works if you set `"action"` in the manifest (or else throw an error).
+ * If you set "default_popup" in the manifest's "action" block, the error is not throw but
+ * the listener is not called neither.
  */
 export function addOnActionClickedListener(
 	callback: (tab: chrome.tabs.Tab) => void,
